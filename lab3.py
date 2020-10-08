712be6e9b2a92834e9ecb63bba271638deedc2ae
Joshua Walsh <joshwalsh12@icloud.com>
2020-10-07 19:05:44 -0700

    fixed encryption edgecase as well as finishing decryption almost

--- lab3.py
+++ lab3.py
@@ -1,66 +1,100 @@
-print('Would you like to: ')
-print('(1) encrypt')
-print('(2) decrypt')
-shouldEncrypt=input()
-print('Please enter a key: ')+key=input()
-if shouldEncrypt=='1':
-    print('Please enter a message to encrypt:')
-    message=input()
-    encryptedMessage=encryptMessage(key,message)
-    print('Here is your encrypted message: ')
-    print(encryptedMessage)
-elif shouldEncrypt=='2':
-    print('Please enter a message to decrypt: ')
-    message=input()
-    decryptedMessage=decryptMessage(key,message)
-    print('Here is your decrypted message: ')
-    print(decryptedMessage)
-
 def decryptMessage(key,message):
+    keySeperater=list(key)
+    messageSeperater=list(message)
+    line=createLine(keySeperater)
+    finalDecryptedMessage=groupDecryptedMessage(messageSeperater,line)
+    return finalDecryptedMessage
 
 def encryptMessage(key,message):
     keySeperater=list(key)
     messageSeperater=list(message)
     line=createLine(keySeperater)
-    groupMessage(messageSeperater,line)
+    finalEncryptedMessage=groupEncryptedMessage(messageSeperater,line)
+    return finalEncryptedMessage
 
-def groupMessage(messageSeperater,line)
+
+def groupDecryptedMessage(messageSeperater, line):
     index=0
+    messageDecrypted=''
+    while index<len(messageSeperater):
+        firstLetter= messageSeperater[index]
+        secondLetter= messageSeperater[index+1]
+        rowOfFirstLetter=defineRow(line.index(firstLetter))
+        columnOfFirstLetter=defineColumn(line.index(firstLetter))
+        rowOfSecondLetter=defineRow(line.index(secondLetter))
+        columnOfSecondLetter=defineColumn(line.index(secondLetter))
+        if rowOfFirstLetter==rowOfSecondLetter:
+            columnOfFirstLetter=columnOfFirstLetter-1
+            columnOfSecondLetter=columnOfSecondLetter-1
+            if columnOfFirstLetter==0:
+                columnOfFirstLetter=5
+            if columnOfSecondLetter==0:
+                columnOfSecondLetter=5
+        elif columnOfFirstLetter==columnOfSecondLetter:
+            rowOfFirstLetter=rowOfFirstLetter-1
+            rowOfSecondLetter=rowOfSecondLetter-1
+            if rowOfFirstLetter==0:
+                rowOfSecondLetter=5
+            if rowOfSecondLetter==0:
+                rowOfSecondLetter=5
+        else: 
+            firstLetterSquareColumn=columnOfSecondLetter
+            secondLetterSquareColumn=columnOfFirstLetter
+            columnOfFirstLetter=firstLetterSquareColumn
+            columnOfSecondLetter=secondLetterSquareColumn
+        indexOfFirstLetter=int((rowOfFirstLetter-1)*5+columnOfFirstLetter-1)
+        indexOfSecondLetter=int((rowOfSecondLetter-1)*5+columnOfSecondLetter-1)
+        index+=2
+        decryptedFirstLetter=line[indexOfFirstLetter]
+        print('indexOfFirstLetter'+str(indexOfFirstLetter))
+        decryptedSecondLetter=line[indexOfSecondLetter]
+        messageDecrypted+=decryptedFirstLetter+decryptedSecondLetter
+    return messageDecrypted
+    
+
+def groupEncryptedMessage(messageSeperater, line):
+    index=0
+    messageEncrypted=''
     while index<len(messageSeperater):
         firstLetter= messageSeperater[index]
         secondLetter=''
         if len(messageSeperater)%2==0 or index!=len(messageSeperater)-1:
             secondLetter= messageSeperater[index+1]
         else:
-            secondLetter='X'
-        rowOfFirstLetter=defineRow(firstLetter)
-        columnOfFirstLetter=defineColumn(firstLetter)
-        rowOfSecondLetter=defineRow(secondLetter)
-        columnOfSecondLetter=defineColumn(secondLetter)
+            secondLetter='x'
+        if (firstLetter==secondLetter):
+            secondLetter='x'
+        rowOfFirstLetter=defineRow(line.index(firstLetter))
+        columnOfFirstLetter=defineColumn(line.index(firstLetter))
+        rowOfSecondLetter=defineRow(line.index(secondLetter))
+        columnOfSecondLetter=defineColumn(line.index(secondLetter))
         if rowOfFirstLetter==rowOfSecondLetter:
             columnOfFirstLetter=columnOfFirstLetter+1
             columnOfSecondLetter=columnOfSecondLetter+1
-            if columnOfSecondLetter=6:
+            if columnOfFirstLetter==6:
                 columnOfFirstLetter=1
-                rowOfSecondLetter=rowOfSecondLetter+1
-                if rowOfSecondLetter=6:
-                    rowOfSecondLetter=1
-        if columnOfFirstLetter==columnOfSecondLetter:
+            if columnOfSecondLetter==6:
+                columnOfSecondLetter=1
+        elif columnOfFirstLetter==columnOfSecondLetter:
             rowOfFirstLetter=rowOfFirstLetter+1
             rowOfSecondLetter=rowOfSecondLetter+1
+            if rowOfFirstLetter==6:
+                rowOfFirstLetter=1
             if rowOfSecondLetter==6:
                 rowOfSecondLetter=1
-                columnOfSecondLetter=columnOfFirstLetter+1
-                if columnOfSecondLetter=6:
-                    columnOfSecondLetter=1
         else: 
             firstLetterSquareColumn=columnOfSecondLetter
             secondLetterSquareColumn=columnOfFirstLetter
             columnOfFirstLetter=firstLetterSquareColumn
             columnOfSecondLetter=secondLetterSquareColumn
-        messageString=((rowOfFirstLetter-1)*5+columnOfFirstLetter=indexOfFirstLetter-1,(rowOfSecondLetter-1)*5+columnOfSecondLetter=indexOfSecondLetter-1)
+        indexOfFirstLetter=int((rowOfFirstLetter-1)*5+columnOfFirstLetter-1)
+        indexOfSecondLetter=int((rowOfSecondLetter-1)*5+columnOfSecondLetter-1)
         index+=2
-    
+        encryptedFirstLetter=line[indexOfFirstLetter]
+        encryptedSecondLetter=line[indexOfSecondLetter]
+        messageEncrypted+=encryptedFirstLetter+encryptedSecondLetter
+    return messageEncrypted 
+        
 def createLine(keySeperater):
     alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','v','w','x','y','z',' ']
     array=keySeperater+alphabet
@@ -68,7 +102,28 @@ def createLine(keySeperater):
     return array
 
 def defineRow(index):
-    return index/5
+    return int(index/5)+1
 
 def defineColumn(index):
-    return index%5
+    return int(index%5)+1
+
+print('Would you like to: ')
+print('(1) encrypt')
+print('(2) decrypt')
+shouldEncrypt=input().replace('u','v').replace('q','k')
+print('Please enter a key: ')
+key=input().lower()
+if shouldEncrypt=='1':
+    print('Please enter a message to encrypt:')
+    message=input().lower().replace('u','v').replace('q','k')
+    encryptedMessage=encryptMessage(key,message)
+    print('Here is your encrypted message: ')
+    print(encryptedMessage)
+elif shouldEncrypt=='2':
+    print('Please enter a message to decrypt: ')
+    message=input().lower().replace('u','v').replace('q','k')
+    decryptedMessage=decryptMessage(key,message)
+    print('Here is your decrypted message: ')
+    print(decryptedMessage)
+
+
